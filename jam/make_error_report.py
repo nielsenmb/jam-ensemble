9fa1b722b191398d0d38ac8848c7daec0ccefb7a
alexlyttle <a.j.lyttle@outlook.com>
2020-02-17 17:48:04 +0000

    Draft script to report on failed targets

---
+++ jam/make_error_report.py
@@ -0,0 +1,8 @@
+'''
+TODO:
+This file generates a report of failed targets and the reason why they failed.
+It then produces a new pbjam input csv containing all the failed targets
+if a rerun is required -> or the user chooses which failures they wish to
+rerun and which to discard. It could also give advice on dealing with 
+certain exceptions (e.g. expanding the prior through bw_fac).
+'''
